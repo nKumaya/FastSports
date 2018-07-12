417e9e989061b17d5d6550c607ac437676b023cb
Ishikawa Miteki <miteki@IshikawanoMacBook-Air.local>
2018-07-12 23:37:32 +0900

    AIDialog -> HogeActivityに移植

--- app/src/main/java/com/example/miteki/fastsports/HogeActivity.java
+++ app/src/main/java/com/example/miteki/fastsports/HogeActivity.java
@@ -4,18 +4,40 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.miteki.fastsports.common.data.fixtures.MessageFixtures;
 import com.example.miteki.fastsports.model.Message;
 import com.example.miteki.fastsports.utils.AppUtils;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.stfalcon.chatkit.messages.MessageInput;
 import com.stfalcon.chatkit.messages.MessagesList;
 import com.stfalcon.chatkit.messages.MessagesListAdapter;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import ai.api.android.AIConfiguration;
+import ai.api.android.GsonFactory;
+import ai.api.model.AIError;
+import ai.api.model.AIResponse;
+import ai.api.model.Metadata;
+import ai.api.model.Result;
+import ai.api.model.Status;
+import ai.api.ui.AIDialog;
+
 public class HogeActivity extends DemoMessagesActivity
         implements MessageInput.InputListener,
-        MessageInput.AttachmentsListener{
+        MessageInput.AttachmentsListener,
+        AIDialog.AIDialogListener{
+
+    private static final String TAG = AIDialogActivity.class.getName();
+    private AIDialog aiDialog;
+    private Gson gson = GsonFactory.getGson();
 
     public static void open(Context context) {
         context.startActivity(new Intent(context, HogeActivity.class));
@@ -24,7 +46,7 @@ public class HogeActivity extends DemoMessagesActivity
     private MessagesList messagesList;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hoge);
 
@@ -33,7 +55,15 @@ public class HogeActivity extends DemoMessagesActivity
 
         MessageInput input = (MessageInput) findViewById(R.id.input);
         input.setInputListener(this);
+
+        final AIConfiguration config = new AIConfiguration(Config.ACCESS_TOKEN,
+                AIConfiguration.SupportedLanguages.Japanese,
+                AIConfiguration.RecognitionEngine.System);
+
+        aiDialog = new AIDialog(this, config);
+        aiDialog.setResultsListener(this);
     }
+
 
     @Override
     public void onAddAttachments() {
@@ -45,11 +75,6 @@ public class HogeActivity extends DemoMessagesActivity
     public boolean onSubmit(CharSequence input) {
         super.messagesAdapter.addToStart(
                 MessageFixtures.getTextMessage(input.toString()), true);
-//        try {
-//            Thread.sleep(5000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
         super.messagesAdapter.addToStart(
                 MessageFixtures.getBotTextMessage("もう一度お願いします。"), true);
         return true;
@@ -69,5 +94,90 @@ public class HogeActivity extends DemoMessagesActivity
                     }
                 });
         this.messagesList.setAdapter(super.messagesAdapter);
+    }
+
+    @Override
+    public void onResult(final AIResponse response) {
+//        runOnUiThread(new Runnable() {
+//            @Override
+//            public void run() {
+                Log.d(TAG, "onResult");
+
+//                resultTextView.setText(gson.toJson(response));
+
+                Log.i(TAG, "Received success response");
+
+                // this is example how to get different parts of result object
+                final Status status = response.getStatus();
+                Log.i(TAG, "Status code: " + status.getCode());
+                Log.i(TAG, "Status type: " + status.getErrorType());
+
+                final Result result = response.getResult();
+                Log.i(TAG, "Resolved query: " + result.getResolvedQuery());
+
+
+                Log.i(TAG, "Action: " + result.getAction());
+                final String speech = result.getFulfillment().getSpeech();
+                Log.i(TAG, "Speech: " + speech);
+//                TTS.speak(speech);
+
+                final Metadata metadata = result.getMetadata();
+                if (metadata != null) {
+                    Log.i(TAG, "Intent id: " + metadata.getIntentId());
+                    Log.i(TAG, "Intent name: " + metadata.getIntentName());
+                }
+
+                final HashMap<String, JsonElement> params = result.getParameters();
+                if (params != null && !params.isEmpty()) {
+                    Log.i(TAG, "Parameters: ");
+                    for (final Map.Entry<String, JsonElement> entry : params.entrySet()) {
+                        Log.i(TAG, String.format("%s: %s", entry.getKey(), entry.getValue().toString()));
+                    }
+                }
+//            }
+
+//        });
+
+    }
+
+    @Override
+    public void onError(AIError error) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onCancelled() {
+//        runOnUiThread(new Runnable() {
+//            @Override
+//            public void run() {
+//                resultTextView.setText("");
+//            }
+//        });
+    }
+
+    @Override
+    protected void onPause() {
+        if (aiDialog != null) {
+            aiDialog.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        if (aiDialog != null) {
+            aiDialog.resume();
+        }
+        super.onResume();
+    }
+
+    public void buttonListenOnClick(final View view) {
+        aiDialog.showAndListen();
     }
 }
