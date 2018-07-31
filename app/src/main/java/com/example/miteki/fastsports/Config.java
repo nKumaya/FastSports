633881e7eff0450569261e1a8f69c169f7c0d664
bookun <kutsuzawa2851@gmail.com>
2018-07-31 14:57:23 +0900

    秘匿情報をgit管理しないようにした

--- app/src/main/java/com/example/miteki/fastsports/Config.java
+++ app/src/main/java/com/example/miteki/fastsports/Config.java
@@ -2,10 +2,10 @@ package com.example.miteki.fastsports;
 
 public abstract class Config {
     // copy this keys from your developer dashboard
-    public static final String ACCESS_TOKEN = "97e5f8a97d0f413796e77061c1928069";
+    public static final String ACCESS_TOKEN = BuildConfig.DASHBOARD_ACCESS_TOKEN;
 
     public static final LanguageConfig[] languages = new LanguageConfig[]{
-            new LanguageConfig("ja", "b92617a3f82e4b52b3db44436d2d4b8b"),
+            new LanguageConfig("ja", BuildConfig.LANGUAGE_ACCESS_TOKEN),
     };
 
     public static final String[] events = new String[]{
